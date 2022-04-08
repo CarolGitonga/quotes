d7413df59f1d490f497127a6fd16fa6d1306396e
carol-profile <carolgitonga93@gmail.com>
2022-04-09 00:44:52 +0300

    add the quotes component

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../qoute';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes: Quote[] =[
+    new Quote(1, 'First Quote', 0, 0),
+    new Quote(2, 'Second Quote', 0, 0),
+    new Quote(3, 'Third Quote', 0, 0)
+    
+  ]
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
