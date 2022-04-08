6c559bda81c6b9c02760ed00e2189377025b05bf
carol-profile <carolgitonga93@gmail.com>
2022-04-09 01:21:01 +0300

    make changes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,7 +6,7 @@ import { Quote } from '../qoute';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  quotes: Quote[] =[
+  public quotes: Quote[] =[
     new Quote(1, 'First Quote', 0, 0),
     new Quote(2, 'Second Quote', 0, 0),
     new Quote(3, 'Third Quote', 0, 0)
