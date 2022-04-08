0e68a5a3c6d79b3067a5f5990d2630f58de74b91
carol-profile <carolgitonga93@gmail.com>
2022-04-09 02:31:48 +0300

    make changes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,12 +7,13 @@ import { Quote } from '../qoute';
 })
 export class QuotesComponent implements OnInit {
   public quotes: Quote[] =[
-    new Quote(1, 'First Quote', 0, 0),
-    new Quote(2, 'Second Quote', 0, 0),
+    new Quote(1, 'First Quote', 1, 0),
+    new Quote(2, 'Second Quote', 3, 0),
     new Quote(3, 'Third Quote', 0, 0)
     
   ]
-
+  arr: number[] = this.quotes.map(quote=>quote.upvotes)
+  highest = Math.max(...this.arr)
   
 
   constructor() { }
